bd83840a3e19f9d180b206980ce137a2126bceb0
sushilkundu143 <skundu@lendingclub.com>
2024-12-09 10:47:37 +0530

    git fix build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    tailwindcss()
   ],
   base: '/webtrendmonitor_frontend/',
 })
